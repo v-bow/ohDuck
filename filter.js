7c6351b38a1a0cc7512342a23a23fd792d086cff
Joyce Fang <fang.jo@northeastern.edu>
2022-02-12 20:45:25 -0500

    popup for profanity percentage

--- filter.js
+++ filter.js
@@ -1,15 +1,18 @@
 // bad word filter for the webpage 
+let badWords = 0;
+let elements = [];
 
 // Runs cleanup on the document title
 let cleanup = function(badWordList) {
-	let elements = Array.prototype.slice.call(document.querySelectorAll("p,span,br,h1,h2,h3,h4,h5,h6,strong,em,blockquote,qhr,code,pre,ul,li,ol,mark,ins,del,sup,sub,small,i,b"));
+	elements = Array.prototype.slice.call(document.querySelectorAll("p, span, br, h1, h2, h3, h4, h5, h6, strong, em, blockquote, qhr, code, pre, ul, li, ol, mark, ins, del, sup, sub, small, i, b, a"));
 
 	elements.forEach(function(el){
 		let words = el.textContent.split(' ')
 		words.forEach(function(word){
-			if (badWordList.includes(word)) {
+			if (badWordList.includes(word.toLowerCase())) {
   				el.innerHTML = el.innerHTML.replace(word, "🦆");
-  			}
+          badWords += 1
+  		}
 		})
 
 	});
@@ -28,7 +31,6 @@ let createObserver = function(badWordList) {
 }
  
 
-
 const badWordListUrl = chrome.runtime.getURL("profanity_words.txt");
 fetch(badWordListUrl)
     .then(response => response.text())
@@ -37,4 +39,21 @@ fetch(badWordListUrl)
         createObserver(badWordList)
     });
 
+// Inform the background page that 
+// this tab should have a page-action.
+chrome.runtime.sendMessage({
+  from: 'filter',
+  subject: 'showPageAction',
+});
+
+// Listen for messages from the popup.
+chrome.runtime.onMessage.addListener((msg, sender, response) => {
+  if ((msg.from === 'popup') && (msg.subject === 'DOMInfo')) {
+    var domInfo = {
+      percentage: Math.round(badWords / elements.length * 100) + "%"
+    }
+
+    response(domInfo);
+  }
+});
 
